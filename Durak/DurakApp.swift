58be21410d79f5a02e046910f2c22af80af2e1b7
a <a>
2026-02-20 12:20:42 -0500

    Create initial project

---
+++ Durak/DurakApp.swift
@@ -0,0 +1,17 @@
+//
+//  DurakApp.swift
+//  Durak
+//
+//  Created by Dare on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct DurakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
